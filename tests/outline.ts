f11d682cf5f4a748a1f8bb7d8da2627164568c19
Cassandra Skytt <ccc.skytt@gmail.com>
2024-09-04 13:59:31 +0200

    first commit

---
+++ tests/outline.ts
@@ -0,0 +1,18 @@
+/* test outline
+
+log in 
+    dashboard -> rooms
+        rooms -> GET -> change room number -> PUT
+        rooms -> back
+    dashboard -> clients
+        clients -> create client -> fill out form -> POST
+        clients -> back
+    dashboard -> bills
+        bills -> GET -> edit -> paid -> PUT
+        bills -> back
+    dashboard -> reservations
+        reservations -> GET -> DELETE
+        reservations -> back
+log out
+
+*/
