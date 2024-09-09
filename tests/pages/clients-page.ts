c275790d03e9286a34ed1c7704934ef18d19eea6
Cassandra Skytt <ccc.skytt@gmail.com>
2024-09-09 14:02:18 +0200

    rooms page edited

--- tests/pages/clients-page.ts
+++ tests/pages/clients-page.ts
@@ -1,29 +1,38 @@
 import { expect, type Locator, type Page } from '@playwright/test';
+import { faker } from '@faker-js/faker';
 
-export class LoginPage {
+export class ClientsPage {
   //Attributes
   readonly page: Page;
-  readonly usernameTextfield: Locator; 
-  readonly passwordTextfield: Locator;
-  readonly loginButton: Locator;
+  readonly createClientButton: Locator;
+  readonly nameTextfield: Locator; 
+  readonly emailTextfield: Locator;
+  readonly telephoneTextfield: Locator;
+  
 
   //Const
   constructor(page: Page) {
     this.page = page;
-    this.usernameTextfield = page.locator('input[type="text"]');
-    this.passwordTextfield = page.locator('input[type="password"]');
-    this.loginButton = page.getByRole('button', { name: 'Login' });
+    this.createClientButton = page.getByRole('button', { name: 'Create Client' }); 
+    this.nameTextfield = page.locator('input[type="text"]');
+    this.emailTextfield = page.locator('input[type="email"]');
+    this.telephoneTextfield = page.locator('input[type="telephone]');
   }
 
   // Methods / functions
   async goto() {
-    await this.page.goto(`${process.env.BASE_URL}`);
+    await this.createClientButton.click();
   }
 
-  async performLogin(username: string, password:string) {
+  async createNewClient(username: string, password:string) {
     //fill out the form - 2 textfields and click the submit button
-    await this.usernameTextfield.fill(username);
-    await this.passwordTextfield.fill(password);
-    await this.loginButton.click();
+    const fullName = faker.person.fullName();      
+    const userEmail = faker.internet.email();     
+    const userPhoneNo = faker.phone.number(); 
+
+    await page.locator('div').filter({ hasText: /^Name$/ }).getByRole('textbox').fill(fullName);
+    await page.locator('div').filter({ hasText: /^Email$/ }).getByRole('textbox').fill(userEmail);
+    await page.locator('div').filter({ hasText: /^Telephone$/ }).getByRole('textbox').fill(userPhoneNo);
+    await page.getByText('Save').click();
   }
 }
