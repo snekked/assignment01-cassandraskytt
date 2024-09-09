72c69cf1931250764f4e870d33ba65fa2633176f
Cassandra Skytt <ccc.skytt@gmail.com>
2024-09-09 14:45:26 +0200

    it was clients not rooms edited

--- tests/testsuite01.ts
+++ tests/testsuite01.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from './pages/login-page';
 import { DashboardPage } from './pages/dashboard-page';
+import { ClientsPage } from './pages/clients-page';
 
 test.describe('Test suite 01', () => {
   test('Test case 01', async ({ page }) => {
@@ -10,11 +11,28 @@ test.describe('Test suite 01', () => {
     await loginPage.goto();
     await loginPage.performLogin(`${process.env.TEST_USERNAME}`,`${process.env.TEST_PASSWORD}`)
     await expect(page.getByRole('heading', { name: 'Tester Hotel Overview' })).toBeVisible();
-    await dashboardPage.performLogout();
-    await expect(page.getByRole('heading', { name: 'Login' })).toBeVisible(); 
-    await page.waitForTimeout(5000);
+
   });
 });
+
+test.describe('Test suite 02', () => {
+  test('Test case 02', async ({ page }) => {
+    const dashboardPage = new DashboardPage(page);
+    const clientsPage = new ClientsPage(page);
+
+  }
+  
+  )
+})
+
+
+
+
+/* await dashboardPage.performLogout();
+await expect(page.getByRole('heading', { name: 'Login' })).toBeVisible(); 
+await page.waitForTimeout(5000); 
+*/
+
 
 /*
 test.describe('Test suite 01', () => {
